195ce1ae7fc2f588a04c5bc3e2cc62ea6609d3bd
Mukuze Nelson <mukuzain@gmail.com>
2014-07-01 21:01:43 +0200

    Lastest version  of folder Experiment

---
+++ Experiment/PartsPercentage.m
@@ -0,0 +1,11 @@
+
+fid = fopen('parts_best_percentages.txt');
+Parts_Percentage=[];
+while 1
+    tline=fgetl(fid);
+    if ~ischar(tline)
+        break;
+    end
+    Parts=str2num(tline);
+    Parts_Percentage=[Parts_Percentage; Parts(2:3)];
+end
